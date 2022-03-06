992593a4deb6616b690f7345c7b84d473f7f80c5
Westsi <jamie@rockyroost.com>
2022-03-06 09:02:35 +0000

    login and account system moved to SOLUS-accounts

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 """
 A banking system to integrate with my stock sim and SOLUS.
 """
+import absolute_url
 
 
 def access_account(username):
-    file = 'C:/Users/jamie/Desktop/SOLUS/USERS/' + username.lower() + '.json'
+    file = absolute_url.absolute_url + username.lower() + '.json'
 
     with open(file, 'r') as user_file:
         user_info = user_file.readlines()
